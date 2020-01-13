b232f5b885c523a83ac323fba124dc4ead43a800
Mattia <mattia.orlandi@studio.unibo.it>
2020-01-13 15:45:28 +0100

    ZMQ socket protocol implementation

--- gesture-recog/gesture_recog.py
+++ gesture-recog/gesture_recog.py
@@ -1,76 +1,72 @@
-import socket
 import cv2
-import pickle
+import numpy as np
+import zmq
+import base64
 from concurrent.futures import ThreadPoolExecutor
-from utils.network_utils import recv_until_term
 
 # Ports for both cameras
 DX_PORT = 8000
 SX_PORT = 8001
 
 
-def create_server_socket(tcp_port: int) -> socket:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(('', tcp_port))
-    sock.listen(1)
+def create_socket(context: zmq.Context, tcp_port: int) -> zmq.Socket:
+    sock = context.socket(zmq.PAIR)
+    sock.setsockopt(zmq.LINGER, 0)
+    sock.bind('tcp://*:{:d}'.format(tcp_port))
 
     return sock
 
 
-def accept_clients(server_sock: socket, sock_idx: str) -> socket:
-    client_sock, client_addr = server_sock.accept()
-    msg = '{} client connected: {}'.format(sock_idx, client_addr)
+def accept_clients(sock: zmq.Socket, sock_idx: str):
+    msg = '{} client connected'.format(sock_idx)
+
+    # Send message to client
+    sock.send_string(msg)
     print(msg)
 
+
+def receive_frame_thread(sock: zmq.Socket, sock_idx: str):
     # Send message to client
-    client_sock.send(msg.encode(encoding='utf-8'))
-
-    return client_sock
-
-
-def receive_frame_thread(sock: socket, sock_idx: str):
-    # Send message to client
-    sock.send('Both clients connected, transmission can start'.encode(encoding='utf-8'))
+    sock.send_string('Both clients connected, transmission can start')
 
     while True:
         # Read stream in buffers of 4096 until a termination character '\0' is found
-        serial_frame = recv_until_term(sock, bufsize=4096)
+        serial_frame = sock.recv_string()
+        frame = cv2.imdecode(np.fromstring(base64.b64decode(serial_frame), dtype=np.uint8), 1)
 
-        frame = pickle.loads(serial_frame)
-
-        cv2.imshow('DX', frame)
+        cv2.imshow('{} frame'.format(sock_idx), frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            sock.send('Stop'.encode(encoding='utf-8'))
+            # If 'q' is pressed, the termination signal is sent to the client
+            sock.send_string('\0')
+            cv2.destroyAllWindows()
             break
-        sock.send('Next'.encode(encoding='utf-8'))
 
 
 def main():
-    # Set up server sockets
-    server_socks = {'DX': create_server_socket(DX_PORT), 'SX': create_server_socket(SX_PORT)}
-    print('Waiting on ports {} and {}...'.format(DX_PORT, SX_PORT))
+    try:
+        # Set up zmq context and sockets PAIR
+        context = zmq.Context()
+        socks = {'DX': create_socket(context, DX_PORT), 'SX': create_socket(context, SX_PORT)}
+        print('Waiting on ports {} and {}...'.format(DX_PORT, SX_PORT))
 
-    # Accept connections in a thread pool
-    client_socks = {}
-    with ThreadPoolExecutor() as executor:
-        future_dx = executor.submit(accept_clients, server_socks['DX'], 'DX')
-        #future_sx = executor.submit(accept_clients, server_socks['SX'], 'SX')
-        client_socks['DX'] = future_dx.result()
-        #client_socks['SX'] = future_sx.result()
+        # Accept connections in a thread pool
+        with ThreadPoolExecutor() as executor:
+            executor.submit(accept_clients, socks['DX'], 'DX')
+            #executor.submit(accept_clients, socks['SX'], 'SX')
 
-    print('Both clients connected, transmission can start.')
+        print('Both clients connected, transmission can start.')
 
-    # Receive frames by both clients using threads
-    with ThreadPoolExecutor() as executor:
-        future_dx = executor.submit(receive_frame_thread, client_socks['DX'], 'DX')
-        #future_sx = executor.submit(receive_frame_thread, client_socks['SX'], 'SX')
-        #frame = future_dx.result()
+        # Receive frames by both clients using threads
+        with ThreadPoolExecutor() as executor:
+            executor.submit(receive_frame_thread, socks['DX'], 'DX')
+            #executor.submit(receive_frame_thread, socks['SX'], 'SX')
 
-    # Close sockets
-    for sock in server_socks.values():
-        sock.shutdown(socket.SHUT_RDWR)
-        sock.close()
+    finally:
+        # Closing sockets
+        for sock in socks.values():
+            sock.close()
+        context.term()
+        print('Terminating...')
 
 
 main()
--- gesture-recog/video_capture.py
+++ gesture-recog/video_capture.py
@@ -1,15 +1,71 @@
 import sys
 import argparse
-import socket
+import zmq
 import cv2
-import pickle
+import base64
 import time
-from picamera.array import PiRGBArray
-from picamera import PiCamera
+# from picamera.array import PiRGBArray
+# from picamera import PiCamera
 
 # Camera width and height
 CW = 640
 CH = 480
+
+
+def capture_from_webcam(sock: zmq.Socket):
+    # Initialize camera
+    video_capture = cv2.VideoCapture(0)
+
+    # Camera warm-up
+    time.sleep(0.1)
+
+    while True:
+        # Grab frame from video
+        ret, frame = video_capture.read()
+        frame = cv2.resize(frame, (CW, CH))
+        encoded, buffer = cv2.imencode('.jpg', frame)
+        jpg_as_text = base64.b64encode(buffer)
+        sock.send(jpg_as_text)
+
+        # Try to read the termination signal from a non-blocking recv
+        try:
+            # If the recv succeeds, break from the loop
+            sock.recv_string(flags=zmq.NOBLOCK)
+            print('Termination signal received')
+            break
+        except zmq.Again:
+            pass
+
+    video_capture.release()
+
+
+def capture_from_picamera(sock: zmq.Socket):
+    # Initialize camera
+    '''camera = PiCamera()
+    camera.resolution = (CW, CH)
+    camera.framerate = 32
+    raw_capture = PiRGBArray(camera, size=(CW, CH))'''
+
+    # Camera warm-up
+    time.sleep(0.1)
+
+    '''for capture in camera.capture_continuous(raw_capture, format='bgr', use_video_port=True):
+        # Grab raw NumPy array representing the frame
+        frame = capture.array
+
+        # Send the serialized frame and the termination sequence '\term'
+        serial_frame = pickle.dumps(frame)
+        sock.send(serial_frame)
+        sock.send(b'\term')
+
+        # Clear stream for next frame
+        raw_capture.truncate(0)
+
+        # Read confirmation
+        confirm = sock.recv(4096).decode(encoding='utf-8')
+        print(confirm)
+        if confirm == 'Stop':
+            break'''
 
 
 def main():
@@ -29,44 +85,27 @@ def main():
     if not 1024 <= port <= 65535:
         sys.exit("Argument 'port' must within range [1024, 65535].")
 
-    # Connect to server
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect((ipaddr, port))
+    try:
+        # Connect to server
+        context = zmq.Context()
+        sock = context.socket(zmq.PAIR)
+        sock.setsockopt(zmq.LINGER, 0)
+        sock.connect("tcp://{}:{}".format(ipaddr, port))
 
         # Confirm connection of the client itself
-        msg = sock.recv(4096).decode(encoding='utf-8')
+        msg = sock.recv_string()
         print(msg)
 
         # Confirm connection of the other client
-        msg = sock.recv(4096).decode(encoding='utf-8')
+        msg = sock.recv_string()
         print(msg)
 
-        # Initialize camera
-        camera = PiCamera()
-        camera.resolution = (CW, CH)
-        camera.framerate = 32
-        raw_capture = PiRGBArray(camera, size=(CW, CH))
-
-        # Camera warm-up
-        time.sleep(0.1)
-
-        for capture in camera.capture_continuous(raw_capture, format='bgr', use_video_port=True):
-            # Grab raw NumPy array representing the frame
-            frame = capture.array
-
-            # Send the serialized frame and the termination sequence '\term'
-            serial_frame = pickle.dumps(frame)
-            sock.send(serial_frame)
-            sock.send(b'\term')
-
-            # Clear stream for next frame
-            raw_capture.truncate(0)
-
-            # Read confirmation
-            confirm = sock.recv(4096).decode(encoding='utf-8')
-            print(confirm)
-            if confirm == 'Stop':
-                break
+        capture_from_webcam(sock)
+    finally:
+        # Closing sockets
+        sock.close()
+        context.term()
+        print('Terminating...')
 
 
 main()
