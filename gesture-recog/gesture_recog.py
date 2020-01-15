8f08b45264c582280fe720d811654e6f65714a25
Mattia <mattia.orlandi@studio.unibo.it>
2020-01-15 15:08:27 +0100

    Function selection implemented

--- gesture-recog/gesture_recog.py
+++ gesture-recog/gesture_recog.py
@@ -2,14 +2,30 @@ import cv2
 import numpy as np
 import zmq
 import base64
+from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor
+from typing import Dict, Callable
+import traceback
 
 # Ports for both cameras
 DX_PORT = 8000
 SX_PORT = 8001
 
+# Folders to store images for calibration
+DX_FOLDER = "../calibration/dx/"
+SX_FOLDER = "../calibration/sx/"
+
+# Chessboard size
+BS = (8, 5)
+
 
 def create_socket(context: zmq.Context, tcp_port: int) -> zmq.Socket:
+    """Creates a zmq.PAIR socket
+        :param context: the zmq context
+        :param tcp_port: integer representing the port
+
+        :return sock: the zmq.PAIR socket created"""
+
     sock = context.socket(zmq.PAIR)
     sock.setsockopt(zmq.LINGER, 0)
     sock.bind('tcp://*:{:d}'.format(tcp_port))
@@ -17,32 +33,126 @@ def create_socket(context: zmq.Context, tcp_port: int) -> zmq.Socket:
     return sock
 
 
-def accept_clients(sock: zmq.Socket, sock_idx: str):
-    msg = '{} client connected'.format(sock_idx)
+def accept_client_thread(sock: zmq.Socket, sock_idx: str):
+    """Confirms connection to client by sending a message
+        :param sock: the zmq socket
+        :param sock_idx: the index of the client ('DX'/'SX')"""
 
-    # Send message to client
-    sock.send_string(msg)
-    print(msg)
+    sock.send_string('Connection established with server')
+    print('Connection established with client {}'.format(sock_idx))
 
 
-def receive_frame_thread(sock: zmq.Socket, sock_idx: str):
-    # Send message to client
-    sock.send_string('Both clients connected, transmission can start')
+def user_input() -> int:
+    """Displays a menu of the available actions and asks user's input
+        :return sel: integer representing user's choice"""
+
+    print('')
+    print('=' * 40)
+    print('1 - Collect images for calibration')
+    print('2 - Perform calibration')
+    print('3 - Real time disparity map')
+    print('4 - Exit')
 
     while True:
-        # Read stream in buffers of 4096 until a termination character '\0' is found
+        try:
+            sel = input('Select one of the options [1, 2, 3, 4]: ')
+            sel = int(sel)
+            if sel not in [1, 2, 3, 4]:
+                print('The option inserted is not valid, retry.')
+            else:
+                break
+        except ValueError:
+            print('The option inserted is not numeric, retry.')
+    print('-' * 40)
+    return sel
+
+
+def select_function(sel: int) -> Callable[[Dict[str, zmq.Socket]], None]:
+    """Selects the function corresponding to user's choice
+        :param sel: integer representing user's choice
+
+        :return f: function corresponding to user's choice"""
+    # Switcher dictionary associating a number to a function
+    switcher = {
+        1: capture_images,
+        2: calibrate,
+        3: disp_map
+    }
+    # Get function from switcher dictionary
+    f = switcher.get(sel)
+    return f
+
+
+def capture_images(socks: Dict[str, zmq.Socket]):
+    print('Collecting images of a chessboard for calibration...')
+    # Receive frames by both clients using threads
+    with ThreadPoolExecutor() as executor:
+        executor.submit(capture_images_thread, socks['DX'], 'DX')
+        executor.submit(capture_images_thread, socks['SX'], 'SX')
+    print('Images collected')
+
+
+def capture_images_thread(sock: zmq.Socket, sock_idx: str):
+    # Initialize variables for countdown
+    n_pics, tot_pics = 0, 5
+    n_sec, tot_sec = 0, 4
+    str_sec = '4321'
+    start_time = datetime.now()
+
+    # Loop until 'tot_pics' images are collected
+    while n_pics < tot_pics:
+        # Read frame as a base64 string
         serial_frame = sock.recv_string()
-        frame = cv2.imdecode(np.fromstring(base64.b64decode(serial_frame), dtype=np.uint8), 1)
+        buffer = base64.b64decode(serial_frame)
+        frame = cv2.imdecode(np.fromstring(buffer, dtype=np.uint8), 1)
+
+        # Display counter on screen before saving frame
+        if n_sec < tot_sec:
+            # Draw on screen the current remaining seconds
+            frame = cv2.putText(img=frame,
+                                           text=str_sec[n_sec],
+                                           org=(int(40), int(80)),
+                                           fontFace=cv2.FONT_HERSHEY_DUPLEX,
+                                           fontScale=3,
+                                           color=(255, 255, 255),
+                                           thickness=5,
+                                           lineType=cv2.LINE_AA)
+
+            # If time elapsed is greater than one second, update 'n_sec'
+            time_elapsed = (datetime.now() - start_time).total_seconds()
+            if time_elapsed >= 1:
+                n_sec += 1
+                start_time = datetime.now()
+        else:
+            # When countdown ends, save image to file
+            path = DX_FOLDER if sock_idx == 'DX' else SX_FOLDER
+            cv2.imwrite(path + '{:02d}'.format(n_pics) + '.jpg', frame)
+            n_pics += 1
+            n_sec = 0
+            print('{:d}/{:d} images collected'.format(n_pics, tot_pics))
 
         cv2.imshow('{} frame'.format(sock_idx), frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            # If 'q' is pressed, the termination signal is sent to the client
+        cv2.waitKey(1)  # invocation of non-blocking 'waitKey', required by OpenCV after 'imshow'
+        if n_pics == tot_pics:
+            # If enough images are collected, the termination signal is sent to the client
             sock.send_string('\0')
-            cv2.destroyAllWindows()
-            break
+
+    cv2.destroyAllWindows()
+
+
+# TODO
+def calibrate():
+    pass
+
+
+# TODO
+def disp_map():
+    pass
 
 
 def main():
+    context = None
+    socks = None
     try:
         # Set up zmq context and sockets PAIR
         context = zmq.Context()
@@ -51,15 +161,27 @@ def main():
 
         # Accept connections in a thread pool
         with ThreadPoolExecutor() as executor:
-            executor.submit(accept_clients, socks['DX'], 'DX')
-            #executor.submit(accept_clients, socks['SX'], 'SX')
+            executor.submit(accept_client_thread, socks['DX'], 'DX')
+            executor.submit(accept_client_thread, socks['SX'], 'SX')
 
-        print('Both clients connected, transmission can start.')
+        # Confirm connection to both clients by sending a message
+        msg = 'Connection established with both clients'
+        print(msg)
+        for sock in socks.values():
+            sock.send_string(msg)
 
-        # Receive frames by both clients using threads
-        with ThreadPoolExecutor() as executor:
-            executor.submit(receive_frame_thread, socks['DX'], 'DX')
-            #executor.submit(receive_frame_thread, socks['SX'], 'SX')
+        # User input cycle
+        while True:
+            # Display action menu and ask for user input
+            sel = user_input()
+            # Send user's choice to both clients
+            for sock in socks.values():
+                sock.send_string(str(sel))
+            if sel == 4:
+                break
+            # Select the corresponding function
+            f = select_function(sel)
+            f(socks)
 
     finally:
         # Closing sockets
--- gesture-recog/video_capture.py
+++ gesture-recog/video_capture.py
@@ -4,15 +4,33 @@ import zmq
 import cv2
 import base64
 import time
-# from picamera.array import PiRGBArray
-# from picamera import PiCamera
+from typing import Dict, Callable
+#from picamera.array import PiRGBArray
+#from picamera import PiCamera
 
-# Camera width and height
-CW = 640
-CH = 480
+# Camera size
+CS = (640, 480)
 
 
-def capture_from_webcam(sock: zmq.Socket):
+def select_function(sel: int) -> Callable[[Dict[str, zmq.Socket]], None]:
+    """Selects the function corresponding to user's choice
+        :param sel: integer representing user's choice
+
+        :return f: function corresponding to user's choice"""
+    # Switcher dictionary associating a number to a function
+    switcher = {
+        1: capture_images,
+        2: calibrate,
+        3: disp_map
+    }
+    # Get function from switcher dictionary
+    f = switcher.get(sel)
+    return f
+
+
+def capture_images(sock):
+    print('Collecting images for calibration...')
+
     # Initialize camera
     video_capture = cv2.VideoCapture(0)
 
@@ -22,7 +40,9 @@ def capture_from_webcam(sock: zmq.Socket):
     while True:
         # Grab frame from video
         ret, frame = video_capture.read()
-        frame = cv2.resize(frame, (CW, CH))
+        frame = cv2.resize(frame, CS)
+
+        # Send the frame as a base64 string
         encoded, buffer = cv2.imencode('.jpg', frame)
         jpg_as_text = base64.b64encode(buffer)
         sock.send(jpg_as_text)
@@ -30,42 +50,56 @@ def capture_from_webcam(sock: zmq.Socket):
         # Try to read the termination signal from a non-blocking recv
         try:
             # If the recv succeeds, break from the loop
-            sock.recv_string(flags=zmq.NOBLOCK)
-            print('Termination signal received')
+            sig = sock.recv_string(flags=zmq.NOBLOCK)
+            print('Termination signal received', sig)
             break
         except zmq.Again:
             pass
 
     video_capture.release()
+    print('Images collected')
+
+
+# TODO
+def calibrate():
+    pass
+
+
+# TODO
+def disp_map():
+    pass
 
 
 def capture_from_picamera(sock: zmq.Socket):
     # Initialize camera
-    '''camera = PiCamera()
-    camera.resolution = (CW, CH)
-    camera.framerate = 32
-    raw_capture = PiRGBArray(camera, size=(CW, CH))'''
+    #camera = PiCamera()
+    #camera.resolution = (CW, CH)
+    #camera.framerate = 32
+    #raw_capture = PiRGBArray(camera, size=(CW, CH))
 
     # Camera warm-up
     time.sleep(0.1)
 
-    '''for capture in camera.capture_continuous(raw_capture, format='bgr', use_video_port=True):
+    #for capture in camera.capture_continuous(raw_capture, format='bgr', use_video_port=True):
         # Grab raw NumPy array representing the frame
-        frame = capture.array
+    #    frame = capture.array
 
-        # Send the serialized frame and the termination sequence '\term'
-        serial_frame = pickle.dumps(frame)
-        sock.send(serial_frame)
-        sock.send(b'\term')
+        # Send the frame as a base64 string
+    #    encoded, buffer = cv2.imencode('.jpg', frame)
+    #    jpg_as_text = base64.b64encode(buffer)
+    #    sock.send(jpg_as_text)
 
         # Clear stream for next frame
-        raw_capture.truncate(0)
+    #    raw_capture.truncate(0)
 
-        # Read confirmation
-        confirm = sock.recv(4096).decode(encoding='utf-8')
-        print(confirm)
-        if confirm == 'Stop':
-            break'''
+        # Try to read the termination signal from a non-blocking recv
+    #    try:
+            # If the recv succeeds, break from the loop
+    #        sig = sock.recv_string(flags=zmq.NOBLOCK)
+    #        print('Termination signal received', sig)
+    #        break
+    #    except zmq.Again:
+    #        pass
 
 
 def main():
@@ -93,14 +127,22 @@ def main():
         sock.connect("tcp://{}:{}".format(ipaddr, port))
 
         # Confirm connection of the client itself
-        msg = sock.recv_string()
-        print(msg)
+        print(sock.recv_string())
 
         # Confirm connection of the other client
-        msg = sock.recv_string()
-        print(msg)
+        print(sock.recv_string())
 
-        capture_from_webcam(sock)
+        # User input cycle
+        while True:
+            print('Waiting for user input...')
+            # Read user's choice
+            sel = int(sock.recv_string())
+            if sel == 4:
+                break
+            # Select corresponding function
+            f = select_function(sel)
+            f(sock)
+
     finally:
         # Closing sockets
         sock.close()
